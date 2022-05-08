d9a7ed5af8131e0e1758f5f7872741ce8427e52b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-08 18:30:04 +0200

    dodano rozwiazanie zadania do Task1

--- 2022/part1_may_2022/task1/Task1.scala
+++ 2022/part1_may_2022/task1/Task1.scala
@@ -1,1 +1,23 @@
-// todo
+object Task1 {
+  def main(args: Array[String]) = {
+    def fact1(n: Int): Int = {
+      @scala.annotation.tailrec
+      def fact2(acc: Int, n: Int): Int = {
+        if (n < 0) {
+          throw new ArithmeticException("n must be >= 0")
+        } else if (n == 0) {
+          acc
+        } else {
+          fact2(acc * n, n - 1)
+        }
+      }
+      fact2(1, n)
+    }
+
+    val nums: Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+    println("-" * 50)
+    nums.foreach { i => println(s"factorial(${i}) = ${fact1(i)}") }
+    println("-" * 50)
+  }
+}
