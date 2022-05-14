5cb850f2459a12ad004386049a0f7c347fd9f07f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-14 17:31:26 +0200

    dodano rozwiazanie zadania Task2.7

---
+++ 2022/part1_may_2022/task2/Task2_7.scala
@@ -0,0 +1,27 @@
+object Task2_7 {
+  def main(args: Array[String]) = {
+
+    val nums: Array[Double] = Array(1, 6, 23, 8, 4, 98, 3, 7, 3, 98, 4, 98)
+
+    def getAvgOfNumsAtEvenIndexes(someNums: Array[Double]): Double = {
+      if (someNums.length < 3) {
+        throw new ArithmeticException("No even indexes => no average")
+      } else {
+        var numsAtEvenInds: Array[Double] = Array()
+        for (i <- 2 until someNums.length by 2) {
+          numsAtEvenInds :+= someNums(i)
+        }
+        numsAtEvenInds.sum / numsAtEvenInds.length
+      }
+    }
+
+    println("-" * 50)
+    println("Task 2.7")
+    println("Given the numbers:")
+    println(nums.mkString(", "))
+    println("The average of numbers at even indexes is:")
+    println(getAvgOfNumsAtEvenIndexes(nums))
+    println("That's it goodbye!")
+    println("-" * 50)
+  }
+}
