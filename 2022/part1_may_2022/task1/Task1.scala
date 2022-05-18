8719a97fc7508628af7827fd61c7a380626fccb0
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-18 11:51:01 +0200

    kosmetyka

--- 2022/part1_may_2022/task1/Task1.scala
+++ 2022/part1_may_2022/task1/Task1.scala
@@ -2,20 +2,28 @@ object Task1 {
   def main(args: Array[String]): Unit = {
     def fact1(n: Int): Int = {
       @scala.annotation.tailrec
-      def fact2(acc: Int, n: Int): Int = {
+      def factHelper(acc: Int, n: Int): Int = {
         if (n < 0) {
           throw new ArithmeticException("n must be >= 0")
         } else if (n == 0) {
           acc
         } else {
-          fact2(acc * n, n - 1)
+          factHelper(acc * n, n - 1)
         }
       }
-      fact2(1, n)
+      factHelper(1, n)
+    }
+
+    def fact2(n: Int): Int = n match {
+      case n if (n < 0)  => throw new ArithmeticException("n must be >= 0")
+      case n if (n == 0) => 1
+      case _             => Range(1, n + 1).reduce((a, b) => a * b)
     }
 
     println("-" * 50)
-    Range(0, 12).foreach { i => println(s"factorial(${i}) = ${fact1(i)}") }
+    Range(0, 12).foreach { i => println(s"fact1(${i}) = ${fact1(i)}") }
+    println("-" * 50)
+    Range(0, 12).foreach { i => println(s"fact2(${i}) = ${fact2(i)}") }
     println("-" * 50)
   }
 }
