43f017c5d0f43f8a3d561580af3fa7cdfd919078
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-19 22:12:05 +0200

    kosmetyka

--- 2022/part1_may_2022/task5/Task5_1.scala
+++ 2022/part1_may_2022/task5/Task5_1.scala
@@ -5,20 +5,20 @@ import java.text.SimpleDateFormat
 object Task5_1 {
   def main(args: Array[String]): Unit = {
 
-    val DaysOfWeek: Map[String, String] = Map(
-      "1" -> "Poniedziałek",
-      "2" -> "Wtorek",
-      "3" -> "Środa",
-      "4" -> "Czwartek",
-      "5" -> "Piątek",
-      "6" -> "Sobota",
-      "7" -> "Niedziela"
+    val DaysOfWeek: Map[Int, String] = Map(
+      1 -> "Poniedziałek",
+      2 -> "Wtorek",
+      3 -> "Środa",
+      4 -> "Czwartek",
+      5 -> "Piątek",
+      6 -> "Sobota",
+      7 -> "Niedziela"
     )
 
     val now: Date = Calendar.getInstance().getTime
     val dowFormater: SimpleDateFormat = new SimpleDateFormat("u")
     // 1 - Monday, ..., 7 - Sunday
-    val dowInt: String = dowFormater.format(now)
+    val dowInt: Int = dowFormater.format(now).toInt
 
     println("-" * 50)
     println("Task 5.1")
