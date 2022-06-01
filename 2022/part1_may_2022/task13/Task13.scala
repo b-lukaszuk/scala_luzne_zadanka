923c00df2900e38f5c75b26d78931a6eec41cd8c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-01 23:12:03 +0200

    dodano rozwiazanie zadania Task13

--- 2022/part1_may_2022/task13/Task13.scala
+++ 2022/part1_may_2022/task13/Task13.scala
@@ -1,7 +1,38 @@
 object Task13 {
   def main(args: Array[String]): Unit = {
+
+    def pigLatWord(word: String): String = {
+      word.substring(1) + word(0) + "ay"
+    }
+
+    def dePigLatWord(word: String): String = {
+      var core: String = word.substring(0, word.length - 2)
+      core(core.length - 1) + core.substring(0, core.length - 1)
+    }
+
+    // only simple sentences, no commas, dots, colons, etc.
+    def convertSentence(sentence: String, toPigLat: Boolean): String = {
+      if (toPigLat) {
+        sentence.split(" ").map(pigLatWord).mkString(" ")
+      } else {
+        sentence.split(" ").map(dePigLatWord).mkString(" ")
+      }
+    }
+
+    val test1: String = "the quick brown fox"
+    val test2: String = "hetay uickqay rownbay oxfay"
+
     println("-" * 30)
-    println("Task 13")
+    println("Task 13\n")
+    println("Pig latinizing phrase:")
+    println(test1)
+    println("result:")
+    println(convertSentence(test1, true))
+    println("\nDePig latinizing phrase:")
+    println(test2)
+    println("result:")
+    println(convertSentence(test2, false))
+    println("\nThat's all. Goodbye.")
     println("-" * 30)
   }
 }
