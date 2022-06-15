9b96677bc1e8383432c8da1cd17f388f777739c0
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-15 22:19:43 +0200

    dod. tlumaczenie Morse to English (+ troche kosmetyki)

--- 2022/part2_june_2022/task1/Task1.scala
+++ 2022/part2_june_2022/task1/Task1.scala
@@ -44,15 +44,69 @@ object Task1 {
       ' ' -> "\t"
     )
 
+    var morseToEng: HashMap[String, Char] = HashMap(
+      ".-" -> 'A',
+      "-.-." -> 'C',
+      "." -> 'E',
+      "--." -> 'G',
+      ".." -> 'I',
+      "-.-" -> 'K',
+      "--" -> 'M',
+      "---" -> 'O',
+      "--.-" -> 'Q',
+      "..." -> 'S',
+      "..-" -> 'U',
+      ".--" -> 'W',
+      "-.--" -> 'Y',
+      "-----" -> '0',
+      "..---" -> '2',
+      "....-" -> '4',
+      "-...." -> '6',
+      "---.." -> '8',
+      ".-.-.-" -> '.',
+      "-..." -> 'B',
+      "-.." -> 'D',
+      "..-." -> 'F',
+      "...." -> 'H',
+      ".---" -> 'J',
+      ".-.." -> 'L',
+      "-." -> 'N',
+      ".--." -> 'P',
+      ".-." -> 'R',
+      "-" -> 'T',
+      "...-" -> 'V',
+      "-..-" -> 'X',
+      "--.." -> 'Z',
+      ".----" -> '1',
+      "...--" -> '3',
+      "....." -> '5',
+      "--..." -> '7',
+      "----." -> '9',
+      "\t" -> ' '
+    )
+
     def getMorseFromEng(msgEng: String): String = {
       msgEng.toUpperCase
         .map((engLett) => engToMorse.getOrElse(engLett, "?"))
         .mkString(" ");
     }
 
+    def getEngFromMorse(msgMorse: String): String = {
+      msgMorse
+        .split(" ")
+        .map((morseLett) => morseToEng.getOrElse(morseLett, "?"))
+        .mkString("");
+    }
+
+    val engOryg: String = "Ala ma kota";
+    val morseDerived: String = getMorseFromEng(engOryg);
+    var engDerived: String = getEngFromMorse(morseDerived);
+
     println("-" * 50)
-    println("'Ala ma kota' in Morse code is:");
-    println(getMorseFromEng("Ala ma kota"));
+    println(s"'${engOryg}' in Morse code is:");
+    println(morseDerived);
+    println(s"'${morseDerived}' in English is:");
+    println(engDerived);
     println("-" * 50)
   }
 }
