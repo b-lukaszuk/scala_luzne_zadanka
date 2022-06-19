882cc44f55b1b346d0561bb75b1800453482a74b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-19 22:15:40 +0200

    dodano pare funkcji pomocniczych do klasy Matrix (do skonczenia)

--- 2022/part2_june_2022/task4/Matrix.scala
+++ 2022/part2_june_2022/task4/Matrix.scala
@@ -1,7 +1,46 @@
 package matrix
 
-class Matrix {
-  def sayHello(): Unit = {
-    println("Hello from class matrix");
+class Matrix(val matrix: Array[Array[Int]]) {
+
+  private def nRows(): Int = {
+    matrix.length;
+  }
+
+  // assumes all rows got equal length
+  private def nCols(): Int = {
+    matrix(0).length;
+  }
+
+  private def areDimsCompatForMult(other: Matrix): Boolean = {
+    this.nCols == other.nRows;
+  }
+
+  private def dotProduct(arr1: Array[Int], arr2: Array[Int]): Int = {
+    var result: Array[Int] = Array.fill(arr1.length) { 0 };
+    for (i <- 0 to (arr1.length - 1)) {
+      result(i) = arr1(i) * arr2(i)
+    }
+    result.reduce((a, b) => a + b);
+  }
+
+//   private def multiply(other: Matrix) = {}
+
+// https://www.mathsisfun.com/algebra/matrix-multiplying.html
+//   def mult(other: Matrix) = {
+//     if (areDimsCompatForMult(other)) {
+//       // multiply
+//     } else {
+//       throw new ArithmeticException(
+//         "in [r1, c1] * [r2, c2] c1 must be equal to r2"
+//       );
+//     }
+//   }
+
+  override def toString(): String = {
+    matrix
+      .map((row) => {
+        row.map((digit) => "%3d".format(digit)).mkString(", ")
+      })
+      .mkString("\n");
   }
 }
--- 2022/part2_june_2022/task4/Task4.scala
+++ 2022/part2_june_2022/task4/Task4.scala
@@ -3,10 +3,19 @@ import matrix.Matrix;
 object Task4 {
   def main(args: Array[String]): Unit = {
 
-    val matr: Matrix = new Matrix();
+// https://www.mathsisfun.com/algebra/matrix-multiplying.html
+    val matr1: Matrix = new Matrix(
+      Array(Array(1, 2, 3), Array(4, 5, 6))
+    );
+    val matr2: Matrix = new Matrix(
+      Array(Array(7, 8), Array(9, 10), Array(11, 12))
+    );
+    val matr3: Matrix = new Matrix(
+      Array(Array(7, 8, 9), Array(10, 11, 12))
+    );
 
     println("-" * 30);
-    matr.sayHello;
+    println(matr1.toString);
     println("-" * 30);
   }
 }
