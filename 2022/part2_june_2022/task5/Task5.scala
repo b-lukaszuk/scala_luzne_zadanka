559ab868515ca1df7067417982c765352d621914
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-23 22:44:56 +0200

    dodano szkielet/poczatek rozwiazania do Task5

---
+++ 2022/part2_june_2022/task5/Task5.scala
@@ -0,0 +1,18 @@
+object Task5 {
+  def main(args: Array[String]): Unit = {
+
+    def declare(): Unit = {
+      println("Calculating probability of success for:");
+      println(
+        "at least 2 people at a party celebrating their b-days the same day"
+      );
+      println("number of people at the party: 30");
+      println("number of iterations: 10k");
+      println("Please be patient this may take a while");
+    }
+
+    println("Task 5");
+    println("-" * 30);
+    println("-" * 30);
+  }
+}
