a3b95f82cb0ba163de1ad3f5aa722eacc2aa607a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-06-26 07:19:05 +0200

    dod. zgadywnie przez wieznia (getNexGuess, didPrisFoundCard), do skonczenia

--- 2022/part2_june_2022/task6/Task6.scala
+++ 2022/part2_june_2022/task6/Task6.scala
@@ -3,8 +3,11 @@ import scala.util.Random;
 object Task6 {
   def main(args: Array[String]): Unit = {
 
-    def getCardsInDrawers(noOfCards: Int = 100): List[Int] = {
-      Random.shuffle(Range(1, noOfCards).toList);
+    val noOfPrisoners: Int = 10;
+    val noOfCards: Int = 10;
+
+    def getCardsInDrawers(noOfCards: Int = noOfCards): List[Int] = {
+      Random.shuffle(Range(0, noOfCards).toList);
     }
     var cupboard: List[Int] = getCardsInDrawers();
     val noOfSimulations: Int = 10000;
@@ -15,10 +18,45 @@ object Task6 {
       r1
     }
 
+    def getNextGuess(
+        prevGuess: Int,
+        atRandom: Boolean,
+        cardsInCupboard: List[Int] = cupboard
+    ): Int = {
+      if (atRandom) {
+        cardsInCupboard(getRandInt(0, noOfCards));
+      } else {
+        cardsInCupboard(prevGuess);
+      }
+    }
+
+    def didPrisFoundCard(
+        prisId: Int,
+        noOfGuesses: Int,
+        atRandom: Boolean = true
+    ): Boolean = {
+      var result: Boolean = false;
+      var counter: Int = 0;
+      var prevGuess: Int = cupboard(getRandInt(0, noOfCards));
+      while (!result && counter < noOfGuesses) {
+        println(s"cur guess: ${prevGuess}");
+        if (prisId == prevGuess) {
+          result = true;
+        } else {
+          prevGuess = getNextGuess(prevGuess, atRandom);
+          counter += 1;
+        }
+      }
+      result;
+    }
+
     println("Task 6");
     println("-" * 30);
-    println("Random cards in cupboard:");
     println(cupboard.mkString(", "));
+    println("random guesses")
+    didPrisFoundCard(0, 10, true);
+    println("strategical guesses")
+    didPrisFoundCard(0, 10, false);
     println("That's all. Goodbye!");
     println("-" * 30);
   }
