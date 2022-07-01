f2f92f5adb0f329fd4d5a312c79a354431d4be04
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-07-01 22:28:05 +0200

    dodano funkcje konwertujace fahrenheit <-> celsius

--- 2022/part2_june_2022/task7/Task7.scala
+++ 2022/part2_june_2022/task7/Task7.scala
@@ -1,5 +1,14 @@
 object Task7 {
   def main(args: Array[String]): Unit = {
+
+    def fahrenheitToCelsius(fahrenheit: Double): Double = {
+      (5 * (fahrenheit - 32)) / 9
+    }
+
+    def celsiusToFahrenheit(celsius: Double): Double = {
+      (9 * celsius / 5) + 32
+    }
+
     println("Task 7");
     println("-" * 30);
     println("That's all. Goodbye!");
