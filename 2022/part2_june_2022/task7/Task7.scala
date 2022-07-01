11976e5fef99bdb1c72406adebf541441692c4c2
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-07-01 22:29:30 +0200

    dod. fn. printMenu

--- 2022/part2_june_2022/task7/Task7.scala
+++ 2022/part2_june_2022/task7/Task7.scala
@@ -9,6 +9,16 @@ object Task7 {
       (9 * celsius / 5) + 32
     }
 
+    def printMenu(): Unit = {
+      println("-" * 5);
+      println("What do You want to do?")
+      println("1. convert temperature Fahrenheit -> Celsius");
+      println("2. convert temperature Celsius -> Fahrenheit");
+      println("3. quit");
+      println("-" * 5);
+      println("Type Your choice:");
+    }
+
     println("Task 7");
     println("-" * 30);
     println("That's all. Goodbye!");
