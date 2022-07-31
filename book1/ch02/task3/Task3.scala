1096dfb623efe472efe88edf08a5b7e936ca2db1
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-07-31 18:58:00 +0200

    added initial scaffolding for Task3

---
+++ book1/ch02/task3/Task3.scala
@@ -0,0 +1,16 @@
+object Task3 {
+  def main(args: Array[String]): Unit = {
+
+    val gravityMeterPerSecondSquared: Double = 9.8;
+    val bulletSpeedMeterPerSecond: Double = 300;
+    val bulletAngleRadian: Double = 1;
+
+    // how to calculate that, starting point:
+    // https://sciencing.com/calculate-bullet-trajectory-5185428.html
+
+    println("-" * 30);
+    println("Task 3.\n");
+    println("That's all. Goodbye!");
+    println("-" * 30);
+  }
+}
