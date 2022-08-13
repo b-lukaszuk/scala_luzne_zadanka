7806ead3851cbf086364c3a1520f3a94a5151486
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-08-13 20:46:41 +0200

    added solution to Task4

---
+++ book1/ch02/task4/Task4.scala
@@ -0,0 +1,49 @@
+import scala.collection.mutable.HashMap;
+
+object Task4 {
+  def main(args: Array[String]): Unit = {
+
+    // Kepler's third law of planetary motion
+    // p^2 = a^3; where
+    // p - orbital period [years]
+    // a - semi-major axis (kind of longer radius of the elipse) [astronomical units]
+
+    val semiMajorAxesInAU: HashMap[String, Double] = HashMap(
+      "Earth" -> 1,
+      "Mars" -> 1.52368055,
+      "Jupiter" -> 5.2038,
+      "Pluto" -> 39.482
+    );
+
+    def getPeriod(semiMajorAxisInAu: Double): Double = {
+      scala.math.pow(scala.math.pow(semiMajorAxisInAu, 3), 0.5);
+    }
+
+    def printResultsOfCalculationsForPlanets(
+        planetsSemiMajorAxes: HashMap[String, Double]
+    ): Unit = {
+      planetsSemiMajorAxes.foreach { case (key, value) =>
+        println("---");
+        println(s"${key}");
+        println("semi-major axis: %.2f [AU]".format(value));
+        println("orbital period %.2f [years]".format(getPeriod(value)));
+      }
+    }
+
+    def printIntro(): Unit = {
+      println("The program calculates a planets orbital period [years]");
+      println("Based on the planet semi-major axis [astronomical units]");
+      println(
+        "The calculations are based on the Kepler's third law of planetary motion"
+      );
+      println("NO GUARANTEE OF CORRECT RESULTS. Still, let's begin\n");
+    }
+
+    println("-" * 30);
+    println("Task 4.\n");
+    printIntro();
+    printResultsOfCalculationsForPlanets(semiMajorAxesInAU);
+    println("\nThat's all. Goodbye!");
+    println("-" * 30);
+  }
+}
