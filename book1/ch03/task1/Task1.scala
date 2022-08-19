ce0587f2884370bd3a6e9469c40c7fae42d4f8f7
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-08-19 21:36:45 +0200

    added solution to ch03, Task1

---
+++ book1/ch03/task1/Task1.scala
@@ -0,0 +1,56 @@
+object Task1 {
+  def main(args: Array[String]): Unit = {
+
+    var userInputX: Double = 0;
+    var userInputY: Double = 0;
+
+    def calculateDistanceFromX0Y0(x: Double, y: Double): Double = {
+      // c^2 = a^2 + b^2
+      val cSquared: Double =
+        scala.math.pow(x - 0, 2) + scala.math.pow(y - 0, 2);
+      scala.math.pow(cSquared, 0.5);
+    }
+
+    def isPointInUnitCircle(x: Double, y: Double): Boolean = {
+      "%.1f".format(calculateDistanceFromX0Y0(x, y)).toDouble <= 1;
+    }
+
+    def printProgramDescription(): Unit = {
+      println(
+        "Hello. This program will determine does a point given by user is inside the unit circle"
+      );
+      println("(a circle centered on the origin with radius 1)");
+      println("Still, I make no promisses, that the program works correctly");
+      println("Caution. Rounding errors are possible");
+      println(
+        "Caution again. Follow the instructions closely, since, e.g. incorrect input may crash the program\n"
+      );
+    }
+
+    def handleUsersInput(): Unit = {
+      println(
+        "Enter the X coordinate (Double) of a point of interest and press Enter"
+      );
+      userInputX = scala.io.StdIn.readDouble();
+      println(
+        "Enter the Y coordinate (Double) of a point of interest and press Enter"
+      );
+      userInputY = scala.io.StdIn.readDouble();
+    }
+
+    def printResultOfEvaluation(): Unit = {
+      print("\nDoes the point (%.2f, %.2f)".format(userInputX, userInputY));
+      println(
+        s" lies inside the unit circle? ${isPointInUnitCircle(userInputX, userInputY)}"
+      )
+    }
+
+    println("-" * 30);
+    println("Task 1\n");
+    printProgramDescription();
+    handleUsersInput();
+    printResultOfEvaluation();
+    println("\nThat's all. Goodbye!")
+    println("-" * 30);
+  }
+}
