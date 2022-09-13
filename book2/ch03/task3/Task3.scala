91c304f719dca2658ac08fb4bfbb45b93bb65658
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-13 20:39:34 +0200

    dodano rozwiazanie do Task3

---
+++ book2/ch03/task3/Task3.scala
@@ -0,0 +1,11 @@
+import listing3_1._
+
+object Task3 {
+  def main(args: Array[String]) = {
+
+  println("\nDrop function for list from listing 3.1, examples:")
+  List.printDropExamples()
+
+  println("\nThat's all. Goodbye!\n")
+  }
+}
