7198723f353f1b103cad8487806a2490efd0b3df
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-13 18:19:33 +0200

    added solutions to Tasks 1 and 2

---
+++ book2/ch03/task2/Task2.scala
@@ -0,0 +1,11 @@
+import listing3_1._
+
+object Task2 {
+  def main(args: Array[String]) = {
+
+  println("\nTail function for list from listing 3.1")
+  List.printTailExamples()
+
+  println("\nThat's all. Goodbye!\n")
+  }
+}
