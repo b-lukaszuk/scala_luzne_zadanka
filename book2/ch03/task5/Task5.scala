a00c7ca588fc8f2b2c630fe43f9d59f38ca19a10
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-15 19:10:06 +0200

    added solution to Task5

---
+++ book2/ch03/task5/Task5.scala
@@ -0,0 +1,11 @@
+import listing3_1._
+
+object Task5 {
+  def main(args: Array[String]) = {
+
+  println("\nsetHead function for list from listing 3.1, examples:\n")
+  List.printSetHeadExamples()
+
+  println("That's all. Goodbye!\n")
+  }
+}
