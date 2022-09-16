b98e9bb39df3462b39a9d09bad4c3408fe7de825
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-16 15:19:40 +0200

    added solution to Task10

---
+++ book2/ch03/task10/Listing3_1.scala
@@ -0,0 +1,51 @@
+package listing3_1
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil         => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil          => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs)  => x * product(xs)
+  }
+
+  // z in signature replaced with acc
+  // acc stands for accumulator, so it is easier to reason about the foldLeft
+  def foldLeft[A,B](l: List[A], acc: B)(f: (B, A) => B): B = l match {
+    case Nil => acc
+    case Cons(x, Nil) => f(acc, x)
+    case Cons(x1, Cons(x2, xs)) => foldLeft(xs, f(f(acc, x1), x2))(f)
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  val example = Cons(1, Cons(2, Cons(3, Nil)))
+  val example2 = List(1, 2, 3)
+  val total = sum(example)
+
+  def printfoldLeftExamples(): Unit = {
+    val foldLeftExample: Float = foldLeft(Cons(2, Cons(5, Cons(1, Nil))), 100)((acc, b) => acc / b)
+    val foldLeftExample2: Float = foldLeft(Cons(3, Cons(2, Nil)), 66)((acc, b) => acc / b)
+    val foldLeftExample3: Float = foldLeft(Cons(3, Nil), 27)((acc, b) => acc / b)
+
+    println("foldLeft(Cons(2, Cons(5, Cons(1, Nil))), 100)((acc, b) => acc / b)")
+    println(s"Result: ${foldLeftExample}\n")
+
+    println("foldLeft(Cons(3, Cons(2, Nil)), 66)((acc, b) => acc / b)")
+    println(s"Result: ${foldLeftExample2}\n")
+
+    println("foldLeft(Cons(3, Nil), 27)((acc, b) => acc / b)")
+    println(s"Result: ${foldLeftExample3}\n")
+  }
+}
---
+++ book2/ch03/task10/Task10.scala
@@ -0,0 +1,11 @@
+import listing3_1._
+
+object Task10 {
+  def main(args: Array[String]) = {
+
+  println("\nfoldLeft function for list from listing 3.1, examples:\n")
+  List.printfoldLeftExamples()
+
+  println("That's all. Goodbye!\n")
+  }
+}
