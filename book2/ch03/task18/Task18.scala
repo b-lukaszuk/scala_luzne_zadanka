460f6ccd4bfe7b022389a8f9f1262af22d409fe6
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-17 08:03:32 +0200

    cosmetic change

--- book2/ch03/task18/Task18.scala
+++ book2/ch03/task18/Task18.scala
@@ -2,8 +2,7 @@ import listing3_1._
 
 object Task18 {
   def main(args: Array[String]) = {
-
-    println("\nmap function for list from listing 3.1, examples:\n")
+    println("\nMap function for list from listing 3.1, examples:\n")
     List.printMapExamples()
 
     println("That's all. Goodbye!\n")
