716589952052f9f7828f160171c06e17c204b490
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-22 19:25:39 +0200

    added solution to Task26

---
+++ book2/ch03/task26/Task26.scala
@@ -0,0 +1,9 @@
+import tree._
+
+object Task26 {
+  def main(args: Array[String]) = {
+    println("\nTree.maximum examples:\n")
+    Tree.printMaximumExamples()
+    println("That's all. Goodbye!\n")
+  }
+}
---
+++ book2/ch03/task26/Tree.scala
@@ -0,0 +1,33 @@
+package tree
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def maximum(ints: Tree[Int]): Int = ints match {
+    case Leaf(x)             => x
+    case Branch(left, right) => maximum(left).max(maximum(right))
+  }
+
+  val example: Int = maximum(Branch(Leaf(3), Branch(Leaf(2), Leaf(5))))
+  val example2: Int = maximum(Branch(Leaf(13), Branch(Leaf(2), Leaf(5))))
+  val example3: Int = maximum(
+    Branch(Leaf(13), Branch(Leaf(2), Branch(Leaf(15), Leaf(4))))
+  )
+
+  def printMaximumExamples(): Unit = {
+
+    println("maximum(Branch(Leaf(3), Branch(Leaf(2), Leaf(5))))")
+    println(s"result: ${example}\n")
+
+    println("maximum(Branch(Leaf(13), Branch(Leaf(2), Leaf(5))))")
+    println(s"result: ${example2}\n")
+
+    println(
+      "maximum(Branch(Leaf(13), Branch(Leaf(2), Branch(Leaf(15), Leaf(4)))))"
+    )
+    println(s"result: ${example3}\n")
+  }
+}
