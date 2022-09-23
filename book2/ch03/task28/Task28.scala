f6fa212b850aceaeab184f8b1ba68a7989d5b391
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-23 07:13:03 +0200

    added solution to Task28

---
+++ book2/ch03/task28/Task28.scala
@@ -0,0 +1,9 @@
+import tree._
+
+object Task28 {
+  def main(args: Array[String]) = {
+    println("\nTree.map examples:\n")
+    Tree.printMapExamples()
+    println("That's all. Goodbye!\n")
+  }
+}
