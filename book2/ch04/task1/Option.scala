c87dbac67c2d6205d7024d5afcdc82f030b10d02
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-20 20:19:54 +0200

    added my version of flatMap with examples

--- book2/ch04/task1/Option.scala
+++ book2/ch04/task1/Option.scala
@@ -19,4 +19,17 @@ object Option {
     println(s"map(None)((x: Int) => x + 11) = ${example2}")
   }
 
+  def flatMap[A, B](option: Option[A])(f: A => Option[B]): Option[B] =
+    option match {
+      case None    => None
+      case Some(a) => f(a)
+    }
+
+  def printflatMapExamples(): Unit = {
+    val example: Option[Char] = flatMap(Some(2))((_: Int) => Some('c'))
+    val example2: Option[Int] = flatMap(None)((x: Int) => Some(x + 11))
+    println(s"flatMap(Some(2))((_: Int) => Some('c')) = ${example}")
+    println(s"flatMap(None)((x: Int) => Some(x + 11)) = ${example2}")
+  }
+
 }
--- book2/ch04/task1/Task1.scala
+++ book2/ch04/task1/Task1.scala
@@ -2,8 +2,13 @@ import option._
 
 object Task1 {
   def main(args: Array[String]) = {
-    println("\nOption.map examples:\n")
+
+    println("\nOption.map examples:")
     Option.printMapExamples()
+
+    println("\nOption.flatMap examples:")
+    Option.printflatMapExamples()
+
     println("\nThat's all. Goodbye!\n")
   }
 }
