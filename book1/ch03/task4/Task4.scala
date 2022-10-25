7e80d485a41001f9d08915285f19b4f8d0eea723
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-25 21:57:13 +0200

    added TEE calculations + cosmetic changes

--- book1/ch03/task4/Task4.scala
+++ book1/ch03/task4/Task4.scala
@@ -9,27 +9,41 @@ object ActivityLevel extends Enumeration {
 object Task4 {
   import ActivityLevel.ActivityLevel
 
-  val bmrMultipliers: HashMap[ActivityLevel, Double] = HashMap(
+  val BMRMultipliers: HashMap[ActivityLevel, Double] = HashMap(
     ActivityLevel.Sedentary -> 1.2,
     ActivityLevel.LightlyActive -> 1.375,
     ActivityLevel.ModeratelyActive -> 1.55,
     ActivityLevel.VeryActive -> 1.725,
-    ActivityLevel.ExtraActive -> 1.9,
+    ActivityLevel.ExtraActive -> 1.9
   )
 
-  def getBmrKcal(
+  // BMR - Basal Metabolic Rate
+  def getBMRKcal(
       weightKg: Double,
       heightCm: Double,
       ageYrs: Double,
       sexFemale: Boolean
   ): Double = {
-    val BmrBase: Double = 10 * weightKg + 6.25 * heightCm - 5 * ageYrs
-    if (sexFemale) BmrBase - 161
-    else BmrBase + 5
+    val BMRBase: Double = 10 * weightKg + 6.25 * heightCm - 5 * ageYrs
+    if (sexFemale) BMRBase - 161
+    else BMRBase + 5
+  }
+
+  // TEE - Total Energy Expended
+  def getTEEKcal(
+      weightKg: Double,
+      heightCm: Double,
+      ageYrs: Double,
+      sexFemale: Boolean,
+      activityLevel: ActivityLevel
+  ): Double = {
+    val BMR: Double = getBMRKcal(weightKg, heightCm, ageYrs, sexFemale)
+    BMR * BMRMultipliers.getOrElse(activityLevel, 0: Double)
   }
 
   def main(args: Array[String]): Unit = {
-    println(getBmrKcal(90, 180, 30, false))
-    println(getBmrKcal(67, 170, 30, true))
+    println(getBMRKcal(90, 180, 30, false))
+    println(getBMRKcal(67, 170, 30, true))
+    println(BMRMultipliers.getOrElse(ActivityLevel.LightlyActive, 0))
   }
 }
