1b4132264f81c4443323063e35ad520650358802
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-25 21:47:25 +0200

    added bmrMultipliers (HashMap) based on ActivityLevels

--- book1/ch03/task4/Task4.scala
+++ book1/ch03/task4/Task4.scala
@@ -7,6 +7,16 @@ object ActivityLevel extends Enumeration {
 }
 
 object Task4 {
+  import ActivityLevel.ActivityLevel
+
+  val bmrMultipliers: HashMap[ActivityLevel, Double] = HashMap(
+    ActivityLevel.Sedentary -> 1.2,
+    ActivityLevel.LightlyActive -> 1.375,
+    ActivityLevel.ModeratelyActive -> 1.55,
+    ActivityLevel.VeryActive -> 1.725,
+    ActivityLevel.ExtraActive -> 1.9,
+  )
+
   def getBmrKcal(
       weightKg: Double,
       heightCm: Double,
