d1c75bd2d1921242b05f261e5cc602d69fa53aa9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-27 21:20:08 +0200

    cosmetic changes

--- book1/ch03/task4/Nutrients.scala
+++ book1/ch03/task4/Nutrients.scala
@@ -13,10 +13,10 @@ object NutrientsPercentageOfKcal {
   import Nutrients.Nutrients
 
   val percentagesKcalMinMax: HashMap[Nutrients, (Double, Double)] = HashMap(
-      Nutrients.Carbohydrates -> (0.45, 0.65),
-      Nutrients.Proteins -> (0.1, 0.3),
-      Nutrients.Fats -> (0.2, 0.35)
-    )
+    Nutrients.Carbohydrates -> (0.45, 0.65),
+    Nutrients.Proteins -> (0.1, 0.3),
+    Nutrients.Fats -> (0.2, 0.35)
+  )
 
   def getPercentageOfKcal(nutrient: Nutrients): (Double, Double) = {
     percentagesKcalMinMax.getOrElse(nutrient, (0: Double, 0: Double))
