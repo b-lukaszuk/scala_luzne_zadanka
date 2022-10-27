518f5f3d38565bdd85d08471ee90456c3c948878
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-27 21:22:55 +0200

    added few exemplary people (objects class Person) for testing

--- book1/ch03/task4/Task4.scala
+++ book1/ch03/task4/Task4.scala
@@ -1,10 +1,20 @@
+import activityLevel._
 import person._
 
 object Task4 {
   def main(args: Array[String]): Unit = {
     println()
     println("-" * 30)
+    val people: Array[Person] = Array(
+      new Person(68, 170, 20, true, ActivityLevel.ModeratelyActive, -5),
+      new Person(80, 182, 30, false, ActivityLevel.LightlyActive, +2),
+      new Person(95, 182, 40, false, ActivityLevel.Sedentary, -5),
+      new Person(63, 162, 23, true, ActivityLevel.ModeratelyActive, -3)
+    )
     println("Task 4")
+    for (person <- people) {
+      println(person)
+    }
     println("-" * 30)
     println()
   }
