da7c088407924610337e06b321236462ad6f95bd
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-30 20:24:42 +0100

    added solution to Task4

--- book2/ch04/task4/Task4.scala
+++ book2/ch04/task4/Task4.scala
@@ -19,13 +19,24 @@ object Task4 {
   def mkMatcher(pat: String): Option[String => Boolean] =
     pattern(pat) map (p => (s: String) => p.matcher(s).find)
 
+  def bothMatch_2(pat1: String, pat2: String, s: String): Option[Boolean] = {
+    map2(mkMatcher(pat1), mkMatcher(pat2))(
+      (p1: String => Boolean, p2: String => Boolean) => p1(s) && p2(s)
+    )
+  }
+
   def main(args: Array[String]): Unit = {
+
+    val example: Option[Boolean] = bothMatch_2("ala", "kota", "ala ma kota")
+    val example2: Option[Boolean] = bothMatch_2("ala", "kota", "ala ma kotka")
 
     println("\n" + "-" * 30)
     println("Task4")
     println("\nbothMatch_2 examples:\n")
-    println(mkMatcher("ala").get("ala ma kota"))
-    println(s"Result: \n")
+    println("bothMatch_2(\"ala\", \"kota\", \"ala ma kota\")")
+    println(s"Result: ${example}\n")
+    println("bothMatch_2(\"ala\", \"kota\", \"ala ma kotka\")")
+    println(s"Result: ${example2}\n")
     println("That's all. Goodbye!")
     println("-" * 30 + "\n")
   }
