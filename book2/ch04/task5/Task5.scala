fb21ed13f5eeebf3b6c7c4bfb53593e2931f554f
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-31 20:55:00 +0100

    added solution to Task5

---
+++ book2/ch04/task5/Task5.scala
@@ -0,0 +1,36 @@
+import java.util.regex._
+
+object Task5 {
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case None :: _ => None
+    case head :: tail =>
+      head.flatMap((elt: A) =>
+        sequence(tail).map((aList: List[A]) => elt :: aList)
+      )
+    case Nil => Some(Nil)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val example: Option[List[Int]] = sequence(List(Some(1), Some(2)))
+    val example2: Option[List[Int]] = sequence(Nil)
+    val example3: Option[List[Int]] = sequence(List(Some(1), None, Some(2)))
+
+    println("\n" + "-" * 30)
+    println("Task5.")
+    println("Sequence examples.\n")
+
+    println("sequence(List(Some(1), Some(2)))")
+    println(s"Result: ${example}\n")
+
+    println("sequence(Nil)")
+    println(s"Result: ${example2}\n")
+
+    println("sequence(List(Some(1), None, Some(2)))")
+    println(s"Result: ${example3}\n")
+
+    println("That's all. Goodbye!")
+    println("-" * 30 + "\n")
+  }
+}
