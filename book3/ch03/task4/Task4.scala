aef007e85653fe434d83ac17cfe8ab26bd298d90
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-03 18:30:21 +0100

    added solution to Task4

---
+++ book3/ch03/task4/Task4.scala
@@ -0,0 +1,19 @@
+object Task4 {
+  def main(args: Array[String]): Unit = {
+
+    val allTimeZonesIDs: Array[String] = java.util.TimeZone.getAvailableIDs
+    val timeZonesWithAmerica: Array[String] =
+      allTimeZonesIDs.filter(_ contains "America")
+    val timeZonesInAmericaWithoutPrefix: Array[String] =
+      timeZonesWithAmerica.map(_.replace("America/", ""))
+
+    println("-" * 30)
+    println("Task4.\n")
+    println(
+      "Printing all timezones (sorted) in America (prefix 'America/' removed):"
+    )
+    println(timeZonesInAmericaWithoutPrefix.sorted.mkString("\n"))
+    println("\nThat's all. Goodbye!")
+    println("-" * 30)
+  }
+}
