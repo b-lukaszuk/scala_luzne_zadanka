1269bbadec97155d5db101408578363091e5c7a5
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-03 17:37:09 +0100

    cosmetic changes, changed fn name, made it more general

--- book3/ch03/task2/Task2.scala
+++ book3/ch03/task2/Task2.scala
@@ -1,16 +1,16 @@
 object Task2 {
-  def swapEltsOfEvenLengthArray(nums: Array[Int]) = {
+  def swapOddAndEvenEltsOfArray[A](array1: Array[A]): IndexedSeq[A] = {
     // for yield with ifs, see Ichoran answer in:
     // https://users.scala-lang.org/t/if-else-in-for-comprehension-when-calculating-monadic-expression/7301
-    for (i <- 0 until nums.length) yield {
+    for (i <- 0 until array1.length) yield {
       if (i % 2 == 0) {
-        if (i != (nums.length - 1)) {
-          nums(i + 1)
+        if (i != (array1.length - 1)) {
+          array1(i + 1)
         } else {
-          nums(i)
+          array1(i)
         }
       } else {
-        nums(i - 1)
+        array1(i - 1)
       }
     }
   }
@@ -19,17 +19,19 @@ object Task2 {
     println("-" * 30)
     println("Task2.")
     println(
-      s"Swapping elements of array ${Array[Int](1, 2, 3, 4).mkString(", ")}"
+      s"Swapping elements of array: ${Array[Int](1, 2, 3, 4).mkString(", ")}"
     )
     println(
-      s"Result: ${swapEltsOfEvenLengthArray(Array[Int](1, 2, 3, 4)).mkString(", ")}"
+      s"Result: ${swapOddAndEvenEltsOfArray(Array[Int](1, 2, 3, 4)).mkString(", ")}"
     )
     println(
-      s"Swapping elements of array ${Array[Int](1, 2, 3, 4, 5).mkString(", ")}"
+      s"Swapping elements of array: ${Array[Int](1, 2, 3, 4, 5).mkString(", ")}"
     )
     println(
-      s"Result: ${swapEltsOfEvenLengthArray(Array[Int](1, 2, 3, 4, 5)).mkString(", ")}"
+      s"Result: ${swapOddAndEvenEltsOfArray(Array[Int](1, 2, 3, 4, 5)).mkString(", ")}"
     )
+    println(s"Swapping elements of array: ${Array[Char]('a', 'b', 'c').mkString(", ")}")
+    println(s"Result: ${swapOddAndEvenEltsOfArray(Array[Char]('a', 'b', 'c')).mkString(", ")}")
     println("That's all. Goodbye!")
     println("-" * 30)
   }
