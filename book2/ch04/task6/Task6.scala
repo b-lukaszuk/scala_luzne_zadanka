0d32117fc3dec51877d9e06f14758e3fd63864bb
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-15 22:45:29 +0100

    added fn sequence (implemented with traverse) + examples

--- book2/ch04/task6/Task6.scala
+++ book2/ch04/task6/Task6.scala
@@ -13,26 +13,27 @@ object Task6 {
 
   def add1ToPositive(x: Int): Option[Int] = if (x > 0) Some(x + 1) else None
 
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
+
   def main(args: Array[String]): Unit = {
     println("\n" + "-" * 30)
     println("Task6.")
 
-    println("Traverse examples.\n")
-    println("Additional info.\n")
-    println(
-      "def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]]"
-    )
-    println(
-      "def add1ToPositive(x: Int): Option[Int] = if (x > 0) Some(x + 1) else None"
-    )
-    println("Let's begin.\n")
+    println("\n---Traverse examples---")
+    println("Additional info:")
+    println("def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]]")
+    println("def add1ToPositive(x: Int): Option[Int] = if (x > 0) Some(x + 1) else None")
+    println("Examples:")
+    println(s"traverse(List(1, 2, 3))(add1ToPositive) = ${traverse(List(1, 2, 3))(add1ToPositive)}")
+    println(s"traverse(List(1, -2, 3))(add1ToPositive) = ${traverse(List(1, -2, 3))(add1ToPositive)}")
 
-    println(
-      s"traverse(List(1, 2, 3))(add1ToPositive) = ${traverse(List(1, 2, 3))(add1ToPositive)}"
-    )
-    println(
-      s"traverse(List(1, -2, 3))(add1ToPositive) = ${traverse(List(1, -2, 3))(add1ToPositive)}"
-    )
+    println("\n---Sequence (implemented with traverse) examples---")
+    println("Additional info:")
+    println("def sequence[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)")
+    println("Examples:")
+    println(s"sequence(List(Some(1), Some(2))) = ${sequence(List(Some(1), Some(2)))}")
+    println(s"sequence(Nil) = ${sequence(Nil)}")
+    println(s"sequence(List(Some(1), None, Some(2))) = ${sequence(List(Some(1), None, Some(2)))}")
 
     println("\nThat's all. Goodbye!")
     println("-" * 30 + "\n")
