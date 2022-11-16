daac7a6cf4f837e824999626129bdedc1e9ae96a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-16 20:41:28 +0100

    added my version of flatMap with Either

--- book2/ch04/task7/Task7.scala
+++ book2/ch04/task7/Task7.scala
@@ -4,14 +4,29 @@ object Task7 {
     case Left(l1)  => Left(l1)
   }
 
+  def myFlatMap[A, EE >: E, B, E](
+      e: Either[EE, A]
+  )(f: A => Either[EE, B]): Either[EE, B] = e match {
+    case Right(r1) => f(r1)
+    case Left(l1)  => Left(l1)
+  }
+
+  def add1IfPositive(num: Int): Either[String, Int] =
+    if (num > 0) Right(num + 1) else Left("num not greater than 0")
+
   def main(args: Array[String]): Unit = {
     println("\n" + "-" * 30)
-    println("Task7.\n")
+    println("Task7.")
 
-    println("map with Either examples.")
-    println("def myMap[A, B, E](e: Either[E, A])(f: A => B): Either[E, B]")
+    println("\nmap with Either examples:")
     println(s"myMap(Right(5))((x: Int) => x + 6) = ${myMap(Right(5))((x: Int) => x + 6)}")
     println(s"myMap(Left(5))((x: Int) => x + 6) = ${myMap(Left(5))((x: Int) => x + 6)}")
+
+    println("\nflatMap with Either examples:")
+    println(s"myFlatMap(Right(5))(add1IfPositive) = ${myFlatMap(Right(5))(add1IfPositive)}")
+    print(s"myFlatMap(Right(0))(add1IfPositive) = ${myFlatMap(Right(0))(add1IfPositive)}")
+    println(",\nwhere:")
+    println("def add1IfPositive(num: Int): Either[String, Int]")
 
     println("\nThat's all. Goodbye!")
     println("-" * 30 + "\n")
