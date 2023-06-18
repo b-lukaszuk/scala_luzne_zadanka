2af2e76712f7fca3e7db055e1c6b1eaa99d1fd7d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-18 17:41:23 +0200

    updated code in book3/ch02/ from scala2 to scala3

--- book3/ch02/task1/Task1.scala
+++ book3/ch02/task1/Task1.scala
@@ -1,21 +1,18 @@
-object Task1 {
-
+object Task1:
   // exponent must be >= 0
   @scala.annotation.tailrec
   def myPower(base: Int, exponent: Int, acc: Double = 1): Double =
-    exponent match {
+    exponent match
       case exponent if (exponent == 0) => acc
       case exponent if (exponent == 1) => acc * base
       case exponent if (exponent > 1) => myPower(base, exponent - 1, acc * base)
       case _ => throw new ArithmeticException("exponent must be >= 0")
-    }
 
-  def myPow(base: Int, exponent: Int): Double = exponent match {
+  def myPow(base: Int, exponent: Int): Double = exponent match
     case exponent if (exponent < 0) => 1 / myPower(base, exponent * -1, 1)
     case _                          => myPower(base, exponent, 1)
-  }
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     println("\n" + "-" * 30)
     println("Task1.\n")
     println("'myPow(base: Int, exponent: Int): Double' examples:\n")
@@ -25,6 +22,3 @@ object Task1 {
     println(s"myPow(4, 3) = ${myPow(4, 3)}")
     println("\nThat's all. Goodbye!")
     println("-" * 30 + "\n")
-  }
-
-}
