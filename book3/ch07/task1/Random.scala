ce6f45dd5494ba1ce9bbed91752767165f03e010
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-06-21 13:56:26 +0200

    cosmetic changes

--- book3/ch07/task1/Random.scala
+++ book3/ch07/task1/Random.scala
@@ -1,15 +1,21 @@
 package random:
-  var seed:Int = 123
+  var seed:Long = System.currentTimeMillis
   val a:Int = 1664525
   val b:Int = 1013904223
   val n:Int = 32
 
-  def setSeed(newSeed:Int): Unit =
+  def setSeed(newSeed:Long): Unit =
     seed = newSeed
 
   def nextDouble():Double =
     val rndDbl:Double = (seed * a + b) % scala.math.pow(2, n)
-    setSeed(rndDbl.toInt)
+    setSeed(rndDbl.toLong)
     rndDbl
 
   def nextInt():Int = nextDouble().toInt
+
+  def getNInts(n:Int = 5) =
+    for (_ <- 1 to n) yield nextInt()
+
+  def getNDoubles(n:Int = 5) =
+    for (_ <- 1 to n) yield nextDouble()
