d9198413e6c0e67eac476a0ff6b5da972801a628
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-07-17 06:50:31 +0200

    added solution to book3/ch09/task3

---
+++ book3/ch09/task3/task3.scala
@@ -0,0 +1,26 @@
+import scala.io.Source
+import scala.util.matching.Regex
+
+def getFileContents(filePath:String):String =
+  Source.fromFile(filePath).getLines.mkString("\n")
+
+@main def task2: Unit =
+  println()
+  println("-" * 30)
+
+  val htmlFilePath: String = "./test.html"
+
+  println(s"Reading exemplary html file -> $htmlFilePath.")
+  println("Extracting 'src' attributes of all 'img' tags.")
+  println("Printing extracted 'src' attributes.\n")
+
+  val htmlContents:String = getFileContents("./test.html")
+  val imgPattern:Regex = """(?<=<img )(src=\".+?\")""".r
+  for
+    matchedString <- imgPattern.findAllIn(htmlContents)
+  do
+    println(matchedString)
+
+  println("\nThat's all. Goodbye!")
+  println("-" * 30)
+  println()
