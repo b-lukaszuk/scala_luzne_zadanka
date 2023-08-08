aac408773768f8c7100a68f5342b518055e049ac
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-08-08 10:47:32 +0200

    cosmetic changes

--- book1/ch05/task5/task5.scala
+++ book1/ch05/task5/task5.scala
@@ -13,20 +13,19 @@ def isPrime(num:Int, div:Int=2): Boolean =
 @tailrec
 def getFactors(num:Int, div:Int, acc:List[Int] = Nil): List[Int] =
   require(num > 0, "Number must be greater than 0.")
-  if (div == 1) then
-    acc // except of 1, because it doesn't go to isPrime
-  else
+  div match
+  case 1 => acc // except of 1, because it doesn't go to isPrime
+  case _ =>
     if (num % div == 0) then
       getFactors(num, div-1, div::acc)
     else
       getFactors(num, div-1, acc)
 
 @tailrec
-def myFilter(pred: (x: Int) => Boolean,
-  l:List[Int], acc:List[Int]=Nil): List[Int] =
-  if l.isEmpty then
-    acc.reverse
-  else
+def myFilter[A](pred: (x: A) => Boolean,
+  l:List[A], acc:List[A]=Nil): List[A] = l match
+  case Nil => acc.reverse
+  case _ =>
     if pred(l.head) then
       myFilter(pred, l.tail, l.head::acc)
     else
@@ -38,14 +37,14 @@ def getPrimeFactors(num:Int): List[Int] =
 @main def task5: Unit =
   println()
   println("-" * 30)
-  println("Recursive functions printing prime factors of a number. Examples.\n")
+  println("Recursive functions for prime factors of a number. Examples.\n")
 
   val rand:scala.util.Random = new scala.util.Random
   val nums: IndexedSeq[Int] = (for (_ <- 1 to 5)
     yield rand.between(2, 100)).sorted
 
-  nums.foreach((n) => 
-    print("Prime factors for: ") 
+  nums.foreach((n) =>
+    print("Prime factors of: ")
     println(s"$n => ${getPrimeFactors(n).mkString("[", ", ", "]")}")
     )
 
