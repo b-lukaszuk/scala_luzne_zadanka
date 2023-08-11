8c6ce74757b6966300caccf9a4c501b86d33e879
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-08-12 00:11:25 +0200

    added solution to book1/ch05/task7/

---
+++ book1/ch05/task7/task7.scala
@@ -0,0 +1,36 @@
+import scala.annotation.tailrec
+import scala.concurrent.duration.fromNow
+
+@tailrec
+def getTuition(start:Double, incr:Double, years: Int):Double =
+    if years == 0 then
+       start
+    else
+       getTuition(start * (1 + incr), incr, years-1)
+
+@tailrec
+def getTotalTuition(start: Double, incr:Double=0.05,
+    years: Int=4, acc:Double=0.0): Double =
+    if years == 0 then
+       acc
+    else
+       getTotalTuition(getTuition(start, incr, 1), incr, years - 1, start + acc)
+
+@main def task5: Unit =
+      println()
+      println("-" * 30)
+      println("Recursive functions for calculating total tuition. Examples.\n")
+
+      println("Start tuition: $30,000/year. Increase 5%/year.")
+      println("Total cost (4-year degree started 10 years from now).\n")
+
+      val startTuition: Double = "%.2f".
+          format(getTuition(30000.0, 0.05, 10)).toDouble
+      val totalTuition: Double = "%.2f".
+          format(getTotalTuition(startTuition)).toDouble
+      println("Start tuition (10 yrs from now) %,.2f usd.".format(startTuition))
+      println(s"Total tuition (after 4 yrs) %,.2f usd.".format(totalTuition))
+
+      println("\nThat's all. Goodbye!")
+      println("-" * 30)
+      println()
