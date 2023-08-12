f6ace42a427378992d07788eb981f4fce83f8b91
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-08-12 11:00:55 +0200

    cosmetic changes

--- book1/ch05/task7/task7.scala
+++ book1/ch05/task7/task7.scala
@@ -3,7 +3,7 @@ import scala.concurrent.duration.fromNow
 
 @tailrec
 def getTuition(start:Double, incr:Double, years: Int):Double =
-    if years == 0 then
+    if years <= 0 then
        start
     else
        getTuition(start * (1 + incr), incr, years-1)
@@ -11,7 +11,7 @@ def getTuition(start:Double, incr:Double, years: Int):Double =
 @tailrec
 def getTotalTuition(start: Double, incr:Double=0.05,
     years: Int=4, acc:Double=0.0): Double =
-    if years == 0 then
+    if years <= 0 then
        acc
     else
        getTotalTuition(getTuition(start, incr, 1), incr, years - 1, start + acc)
@@ -24,10 +24,8 @@ def getTotalTuition(start: Double, incr:Double=0.05,
       println("Start tuition: $30,000/year. Increase 5%/year.")
       println("Total cost (4-year degree started 10 years from now).\n")
 
-      val startTuition: Double = "%.2f".
-          format(getTuition(30000.0, 0.05, 10)).toDouble
-      val totalTuition: Double = "%.2f".
-          format(getTotalTuition(startTuition)).toDouble
+      val startTuition: Double = getTuition(30000.0, 0.05, 10)
+      val totalTuition: Double = getTotalTuition(startTuition)
       println("Start tuition (10 yrs from now) %,.2f usd.".format(startTuition))
       println(s"Total tuition (after 4 yrs) %,.2f usd.".format(totalTuition))
 
